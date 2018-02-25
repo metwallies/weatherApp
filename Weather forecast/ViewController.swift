e13f0e00c37a795f14d8ca632f84094b06ec0701
islam metwally <islam.metwally@vodafone.com>
2018-02-25 12:08:09 +0200

    Initial Commit

---
+++ Weather forecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather forecast
+//
+//  Created by islam metwally on 2/25/18.
+//  Copyright © 2018 Islam Metwally. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
