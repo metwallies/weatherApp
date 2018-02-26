1201792a161d7e69e18f0132cdab9ed92e32a0ab
Islam Metwally <islam.metwall93@gmail.com>
2018-02-26 12:06:05 +0200

    adding models to core date adding core data managers.

---
+++ Weather forecast/CoreDataManager.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataManager.swift
+//  Weather forecast
+//
+//  Created by Islam Metwally on 2/26/18.
+//  Copyright © 2018 Islam Metwally. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataManager: NSObject {
+
+    static func saveCityToFavorites(_ city : City) {
+        //save city to favorites
+    }
+    
+    static func loadFavoriteCities() -> [City] {
+        //TODO: load favorite cities
+    }
+}
